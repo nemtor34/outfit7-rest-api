2722cf4deee50d439abf3a9e7b726c46786f976e
Nemanja Torbica <nemanja@pop-os.localdomain>
2021-11-15 15:52:52 +0100

    Initial commit

---
+++ src/main/java/com/fun7/backend/Fun7BackendApplication.java
@@ -0,0 +1,13 @@
+package com.fun7.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fun7BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fun7BackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fun7/backend/Fun7BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fun7.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fun7BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
